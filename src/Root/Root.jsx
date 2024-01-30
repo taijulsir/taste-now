2f9efa839f162d6a2c9625d8a041d8e6bafc0b0a
taijulsir <princetaijul123@gmail.com>
2024-01-30 15:55:58 +0600

    Create footer section

--- src/Root/Root.jsx
+++ src/Root/Root.jsx
@@ -6,7 +6,7 @@ import Footer from "../Components/Footer/Footer";
 const Root = () => {
     return (
         <div>
-            {/* <Navbar></Navbar> */}
+            <Navbar></Navbar>
             <Outlet></Outlet>
             <Footer></Footer>
         </div>
