6deefa057cfb633a9bc52e252964a81401fd0d09
taijulsir <princetaijul123@gmail.com>
2024-01-30 08:53:47 +0600

    Implement react tab in popular food categories

--- src/Components/PopularCategories/SwiperNavButtons/SwiperNavButtons.jsx
+++ src/Components/PopularCategories/SwiperNavButtons/SwiperNavButtons.jsx
@@ -9,7 +9,6 @@ const SwiperNavButtons = () => {
         <div style={{display:'flex',gap:'150px',justifyContent:"center" }}>
             <button style={{border:'none', background:'none'}} onClick={() => swiper.slidePrev()}><MdArrowBackIos style={{fontSize:'20px',color:"#F72D57"}}></MdArrowBackIos></button>
             <button style={{border:'none', background:'none'}} onClick={() => swiper.slideNext()}><MdArrowForwardIos style={{fontSize:'20px',color:"#F72D57"}}></MdArrowForwardIos></button>
-
         </div>
     );
 };
