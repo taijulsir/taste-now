6ca84e78711cdd4d55d238bd8d04a2fec9b90e80
taijulsir <princetaijul123@gmail.com>
2024-01-30 16:58:06 +0600

    Update footer section and make responsice for mobile device and medium device

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -15,42 +15,49 @@ const Footer = () => {
                         <FaFacebookSquare /> <BsInstagram /> <FaTwitter /> <FaYoutube />
                     </div>
                 </div>
-                {/* 1st service item */}
-                <div>
-                    <ul className="service-container">
-                        <li>Seslendirme ve Alt Jazz</li>
-                        <li>Media Market</li>
-                        <li>Gillie</li>
-                        <li>Size Last</li>
-                    </ul>
-                </div>
 
-                {/* 2ns service item */}
-                <div>
-                    <ul className="service-container">
-                        <li>Self Betimes</li>
-                        <li>Yatırımcı İlişkileri</li>
-                        <li>Basal Himmler</li>
-                    </ul>
-                </div>
 
-                {/* 3rd servie item */}
-                <div>
-                    <ul className="service-container">
-                        <li>Yard Market</li>
-                        <li>Is İmkanları</li>
-                        <li>Car Tercihleri</li>
-                    </ul>
-                </div>
-                {/* 4th service item */}
-                <div>
-                    <ul className="service-container">
-                        <li>Hedge Karla</li>
-                        <li>Mullein Koşulları</li>
-                        <li>Autumnal Bulgier</li>
+                <div className="all-servicies-container">
+                    {/* 1st service item */}
+                    <div>
+                        <ul className="service-container">
+                            <li>Seslendirme ve Alt Jazz</li>
+                            <li>Media Market</li>
+                            <li>Gillie</li>
+                            <li>Size Last</li>
+                        </ul>
+                    </div>
 
-                    </ul>
+                    {/* 2ns service item */}
+                    <div>
+                        <ul className="service-container">
+                            <li>Self Betimes</li>
+                            <li>Yatırımcı İlişkileri</li>
+                            <li>Basal Himmler</li>
+                        </ul>
+                    </div>
+
+                    {/* 3rd servie item */}
+                    <div>
+                        <ul className="service-container">
+                            <li>Yard Market</li>
+                            <li>Is İmkanları</li>
+                            <li>Car Tercihleri</li>
+                        </ul>
+                    </div>
+                    {/* 4th service item */}
+                    <div>
+                        <ul className="service-container">
+                            <li>Hedge Karla</li>
+                            <li>Mullein Koşulları</li>
+                            <li>Autumnal Bulgier</li>
+
+                        </ul>
+                    </div>
                 </div>
+            </div>
+            <div className="small-device-social-links">
+                <FaFacebookSquare /> <BsInstagram /> <FaTwitter /> <FaYoutube />
             </div>
         </footer>
     );
